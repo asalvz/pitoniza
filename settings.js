60d1451f8f25785a765479e8a84c121764a7a0ae
asalvz <114161010+asalvz@users.noreply.github.com>
2022-11-12 11:08:26 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ const erc20list = {
   '0x00a5Dc07A0F8061e896F0Acdd47d352e49FD1a2c': 100, // DAI BSC Testnet (97)
   '0x348236484ce96A293E210260b90bBFb228D6d1Fc': 100, // USDT BSC Testnet (97)
   */
-   Ethernet mainnet
+   // Ethernet mainnet
   '0x6B175474E89094C44Da98b954EedeAC495271d0F': 10, // DAI ethernet
   '0xdAC17F958D2ee523a2206206994597C13D831ec7': 10, // usdt
   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': 10, // usdc
